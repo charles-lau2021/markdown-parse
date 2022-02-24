6c921e65b45e0f3f84c74f7178da4ea70bd83743
IncogOwl <97695545+IncogOwl@users.noreply.github.com>
2022-02-24 14:45:59 -0800

    LabReports

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -129,5 +129,26 @@ public class MarkdownParseTest {
             MarkdownParse.getLinks(str)
         );
     }
+
+    @Test
+    public void testSnippet1() throws IOException {
+        String contents= Files.readString(Path.of("./snippet1.md"));
+        List<String> expect = List.of();
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        String contents= Files.readString(Path.of("./snippet2.md"));
+        List<String> expect = List.of("b.com","example.com");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        String contents= Files.readString(Path.of("./snippet3.md"));
+        List<String> expect = List.of();
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
 }
 
